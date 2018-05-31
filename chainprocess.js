51a1f4f8b3a5e2c17e9b6a36540e6ed22f686ca9
Peter Kiss <ypetya@gmail.com>
2018-05-31 10:46:54 +0200

    initial commit

---
+++ chainprocess.js
@@ -0,0 +1,41 @@
+const cp = require('child_process');
+
+const execute = string => new Promise((resolve, reject)=>{
+    console.log(string);
+
+    cp.exec(string, (err, stdout, stderr) => {
+        if (err) {
+            reject(err);
+        }
+        const out = stdout.toString().trim();
+        console.log(out);
+        console.error(stderr.toString().trim());
+        resolve(out);
+    });
+});
+
+const call = (fn, input) => new Promise((resolve,reject) => {
+    const outcome = fn(input);
+
+    if(typeof outcome === 'string') {
+        console.log(`Output: ${outcome}`);
+    } else if (typeof outcome === 'function') {
+        reject(outcome());
+        return;
+    }
+    resolve(outcome);
+})
+
+const runChild = (goal, input) =>
+    typeof goal === 'string' ?
+        execute(goal) :
+        call(goal, input);
+
+const chainProcess = (goal1, parent) => ({
+    run: input => parent ?
+        parent.run(input).then(o => runChild(goal1, o)) :
+        runChild(goal1,input),
+    chain: childGoal => chainProcess(childGoal, chainProcess(goal1, parent))
+});
+
+export default chainProcess;
